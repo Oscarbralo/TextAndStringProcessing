c427987ad4114d96c710ed5f8ff4e351deab76b2
Alethor <oscarbralo@outlook.com>
2013-09-11 12:39:34 +0200

    Update Program.cs
    Maybe with Regular Expressions will be easier! 
    Check it out! ;) 
    Best regards

--- StringAndTextProcessing/4.CountsSubstring/Program.cs
+++ StringAndTextProcessing/4.CountsSubstring/Program.cs
@@ -8,17 +8,9 @@ class Program
     {
         //string text = "in In";
         string text = "We are living in an yellow submarine. We don't have anything else. Inside the submarine is very tight. So we are drinking all the day. We will move out of it in 5 days.";
-        int counter = 0;
-        int length =text.Length; 
         string substring = "in";
-        for (int i = 0; i < length-1; i++)
-        {
-            //case insensitive, using ToLower to count all matches
-            if (text.Substring(i, 2).ToLower() == substring)
-            {
-                counter++;
-            }
-        }
+        Regex r = new Regex(substring, RegexOptions.IgnoreCase);
+        int counter = r.Matches(text).Count;
         Console.WriteLine("The substring \"{0}\" is contained {1} times in the text.",substring,counter);
     }
 }
